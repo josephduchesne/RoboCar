da00ffe2858dbde0126a8726ebbab4aca8a91435
Joseph Duchesne <josephgeek@gmail.com>
2015-03-26 12:37:25 -0300

    added X11 overrides for pygame and the remote launchs cript

--- ROS_Workspace/scripts/controller_environment.sh
+++ ROS_Workspace/scripts/controller_environment.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export ROS_MASTER_URI=http://pi:11311/
-export ROS_PACKAGE_PATH=/root/RoboCar/ROS_Workspace/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks
+#only needed if a program asks for X11
+#export XAUTHORITY=/home/joseph/.Xauthority 
+#export DISPLAY=:0.0
 source /root/RoboCar/ROS_Workspace/devel/setup.bash
 exec "$@"
