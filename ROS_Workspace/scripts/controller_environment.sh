716d9967d44650a27106ef67f3b31020145ca858
Joseph Duchesne <josephgeek@gmail.com>
2015-03-26 11:21:12 -0300

    fixed joyctl bug

--- ROS_Workspace/scripts/controller_environment.sh
+++ ROS_Workspace/scripts/controller_environment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export ROS_MASTER_URI=http://pi:11311/
+export ROS_PACKAGE_PATH=/root/RoboCar/ROS_Workspace/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks
 source /root/RoboCar/ROS_Workspace/devel/setup.bash
 exec "$@"
--- ROS_Workspace/src/robocar/scripts/joyctl.py
+++ ROS_Workspace/src/robocar/scripts/joyctl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import rospy, pygame
+import rospy, pygame, sys
 
 from geometry_msgs.msg import Twist 
 
@@ -9,24 +9,39 @@ pygame.joystick.init();
 joystick = pygame.joystick.Joystick(0);
 joystick.init();
 
+
 def mover():
   pubUrl = 'twist'
-  pub = rospy.Publisher(pubUrl, Twist, queue_size=10)
   rospy.init_node('joyctl')
+  pub = rospy.Publisher(pubUrl, Twist, queue_size=10)
   rate = rospy.Rate(30) # 1hz
+
+  rospy.loginfo("joyctl start")
+
   while not rospy.is_shutdown():
     twist = Twist();
 
     events = pygame.event.get();
     
-    twist.linear.x = -2 * joystick.get_axis(1);
-    twist.angular.z = -2* joystick.get_axis(0);
+    twist.linear.x = -1 * joystick.get_axis(1);
+    twist.angular.z = -1* joystick.get_axis(0);
 
     pub.publish(twist)
     rate.sleep()
+
+  rospy.loginfo("RosPy shutdown triggered")
+
+STDERR = sys.stderr
+def excepthook(*args):
+  rospy.loginfo("Caught %s" % args)
+
+sys.excepthook = excepthook
 
 if __name__ == '__main__':
   try:
     mover()
   except rospy.ROSInterruptException:
     pass
+
+
+rospy.loginfo("Script naturally exiting?")
