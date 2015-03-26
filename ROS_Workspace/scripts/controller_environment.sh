9bd3cc2848b4e20f86d051ab4a0049386caaeeaa
Joseph Duchesne <josephgeek@gmail.com>
2015-03-26 20:19:41 -0300

    added environment vars to allow ros to launch x11 windows

--- ROS_Workspace/scripts/controller_environment.sh
+++ ROS_Workspace/scripts/controller_environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #only needed if a program asks for X11
-#export XAUTHORITY=/home/joseph/.Xauthority 
-#export DISPLAY=:0.0
+export XAUTHORITY=/home/joseph/.Xauthority 
+export DISPLAY=:0.0
 source /root/RoboCar/ROS_Workspace/devel/setup.bash
 exec "$@"
